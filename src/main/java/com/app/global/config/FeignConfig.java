843ccf57246c8d1ffd5296ef68b26ecdc01454f9
sik <yjs1468@gmail.com>
2023-01-15 21:06:06 +0900

    feign

---
+++ src/main/java/com/app/global/config/FeignConfig.java
@@ -0,0 +1,33 @@
+package com.app.global.config;
+
+import com.app.global.error.FeignClientExceptionErrorDecoder;
+import feign.Logger;
+import feign.Retryer;
+import feign.codec.ErrorDecoder;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClientsConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.app") // todo 패키지명 수정
+@Import(FeignClientsConfiguration.class)
+public class FeignConfig {
+
+    @Bean
+    Logger.Level feginLoggerLevel(){
+        return Logger.Level.FULL;
+    }
+
+    @Bean
+    public ErrorDecoder errorDecoder(){
+        return new FeignClientExceptionErrorDecoder();
+    }
+
+    @Bean
+    public Retryer retryer(){
+        // period 실행주기 // 3 : 최대 몇번의 재시도
+        return new Retryer.Default(1000,2000,3);
+    }
+}
