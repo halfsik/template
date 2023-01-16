1a7cd40638d401a041532bd8a0d86b02acabbb25
sik <yjs1468@gmail.com>
2023-01-16 23:54:19 +0900

    소셜로그인

--- src/main/java/com/app/web/kakaotoken/controller/KakaoTokenController.java
+++ src/main/java/com/app/web/kakaotoken/controller/KakaoTokenController.java
@@ -1,7 +1,10 @@
 package com.app.web.kakaotoken.controller;
 
+import com.app.web.kakaotoken.client.KakaoTokenClient;
+import com.app.web.kakaotoken.dto.KakaoTokenDto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +13,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class KakaoTokenController {
 
+
+    private final KakaoTokenClient client;
+
+    @Value("${kakao.client.id}")
+    private String clientId;
+
+    @Value("${kakao.client.secret}")
+    private String clientSecret;
+
+
     @GetMapping("/login")
     public String login(){
         return "loginForm";
@@ -17,6 +30,18 @@ public class KakaoTokenController {
 
     @GetMapping("/oauth/kakao/callback")
     public @ResponseBody String loginCallback(String code){
-        return code;
+
+        String contentType = "application/x-www-form-urlencoded;charset=utf-8";
+
+        KakaoTokenDto.Request kakoTokenRequestDto = KakaoTokenDto.Request.builder()
+                .client_id(clientId)
+                .client_secret(clientSecret)
+                .grant_type("authorization_code")
+                .code(code)
+                .redirect_uri("http://localhost:8080/oauth/kakao/callback")
+                .build();
+
+        KakaoTokenDto.Response kakoTken = client.reqeuestKakaToken(contentType,kakoTokenRequestDto);
+        return "kako token :"+kakoTken;
     }
 }
